70ca9334d5955c51bb6008b0cd15f068e70c8446
kurochan <kurochan@example.com>
2013-05-15 10:38:23 +0900

    botのタスクを定義できるように

--- kurochan_bot.rb
+++ kurochan_bot.rb
@@ -4,7 +4,10 @@ require "#{PLUGIN_ROOT}/twitter_bot"
 class KurochanBot < TwitterBot
   def initialize
     super
-
     job :intval => 3.seconds, :func => :hello
+    job :intval => 5.minutes, :func => :normal_task
+  end
+
+  def normal_task
   end
 end

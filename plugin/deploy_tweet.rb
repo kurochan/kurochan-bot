813bb79c641ac48b1a92320c5b4d8cde5bd58666
kurochan <kurochan@example.com>
2013-05-22 10:57:18 +0900

    リビジョン番号をHeroku APIから取ってくるように

--- plugin/deploy_tweet.rb
+++ plugin/deploy_tweet.rb
@@ -1,6 +1,13 @@
+require 'heroku-api'
 module DeployTweet
   def deploy_tweet
-    update "version #{self.deploy_time} deployed!"
-    self.deploy_time += 1
+    update "revision #{revision} deployed!" unless revision == deploy_time
+    self.deploy_time = revision
+  end
+
+  def revision
+    api = Heroku::API.new(:api_key => HEROKU_API_KEY)
+    releases = api.get_releases 'kurochan-bot'
+    releases.data[:body][-1]['name'][-1, 1].to_i
   end
 end

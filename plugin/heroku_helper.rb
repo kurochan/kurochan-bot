c79a5aa42faba3a325dc206b4cc32d8653840d5d
kurochan <kurochan@example.com>
2013-05-23 16:02:07 +0900

    リファクタリング

--- plugin/heroku_helper.rb
+++ plugin/heroku_helper.rb
@@ -2,13 +2,14 @@
 module HerokuHelper
   def heroku_revision
     return 0 if (!(defined? HEROKU_API_KEY) || HEROKU_API_KEY == '' || !HEROKU_API_KEY)
+    release = 0
     begin
       api = Heroku::API.new(:api_key => HEROKU_API_KEY)
-      releases = api.get_releases HEROKU_REPO_NAME
-      releases.data[:body][-1]['name'].delete('v').to_i
+      release = api.get_releases(HEROKU_REPO_NAME).
+        data[:body][-1]['name'].delete('v').to_i
     rescue
       puts 'ERROR Cannot access heroku.'
-      return 0
     end
+    return release
   end
 end

40403cafec146f5bf94024d7ada514d55bd67971
kurochan <kuro@kurochan.org>
2014-05-15 12:49:19 +0900

    よるほーの文言変更

--- plugin/yoruho.rb
+++ plugin/yoruho.rb
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 module Yoruho
   def yoruho
+    # 卒業までの日数
+    yday = Time.mktime(2014, 12, 31).yday - Time.new.yday + Time.mktime(2015, 3, 26).yday
     data = [
-      'よるほー♪',
-      'よるほーなやつー♪',
+      "よるほー♪ 卒業まであと#{yday}日だよ！",
+      "卒業まであと#{yday}日だよ！進捗どうですか！",
     ]
     update data[rand data.length]
   end
